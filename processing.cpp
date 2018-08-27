549adfdf730e5577cfd91821036a698e68c14d3a
ASA1976 <rap.paradigm@gmail.com>
2018-08-26 23:30:58 -0400

    Added Procession
    - Added ::procession namespace which provides a basic framework for event queues or stacks.
    - Added processing example (processing.cpp) which demonstrates how to use the procession framework to schedule and process events.
    - Type safety between function and expected data type is addressed when making use of the Schedule... templates (see Function template parameter).

---
+++ processing.cpp
@@ -0,0 +1,65 @@
+// © 2018 Aaron Sami Abassi
+// Licensed under the Academic Free License version 3.0
+// #define RAPBTL_NO_STD_CPLUSPLUS 1
+#include "procession.hpp"
+#include "ration/contraction.hpp"
+#ifndef RAPBTL_NO_STD_CPLUSPLUS
+#include <cstdio>
+#else
+#include <stdio.h>
+#endif
+
+using namespace ::procession;
+using namespace ::ration::contraction;
+
+using Natural = unsigned short;
+
+const Natural
+MaximumEvents = 127;
+
+using EventContractional = Contractional< Natural, MaximumEvents, Contextual >;
+
+constexpr Tractile< EventContractional, Natural, Natural >
+EventContractor = Contractor< Natural, MaximumEvents, Contextual >;
+
+constexpr Directional< EventContractional, Natural, Natural, Contextual >
+EventVisitor = WriteIncrementDirection< Natural, MaximumEvents, Contextual >;
+
+static inline void
+PrintString(
+    const Locational< void >
+        location
+) {
+    using Contextual = const Locational< const char >;
+    printf( "%s\n", (Contextual) location );
+}
+
+static inline void
+PrintNatural(
+    const Locational< void >
+        location
+) {
+    using Contextual = const Locational< const Natural >;
+    printf( "%u\n", *(Contextual) location );
+}
+
+int main() {
+    static auto&
+        ScheduleReadString = ScheduleByPointer< EventContractional, Natural, Natural, const char, EventContractor, EventVisitor, PrintString >;
+    static auto&
+        ScheduleReadNatural = ScheduleByReference< EventContractional, Natural, Natural, const Natural, EventContractor, EventVisitor, PrintNatural >;
+    static auto&
+        ProcessEvent = Process< EventContractional, Natural, Natural, EventContractor, EventVisitor >;
+    static const Locational< const char >
+        Hello = "Hello world!",
+        Goodbye = "Goodbye";
+    static const Natural
+        Size = sizeof( EventContractional );
+    EventContractional
+        queue;
+    Initialize( queue );
+    ScheduleReadString( queue, Hello );
+    ScheduleReadNatural( queue, Size );
+    ScheduleReadString( queue, Goodbye );
+    while (ProcessEvent( queue ));
+}
---
+++ procession.hpp
@@ -0,0 +1,158 @@
+// © 2018 Aaron Sami Abassi
+// Licensed under the Academic Free License version 3.0
+#ifndef PROCESSION_MODULE
+#define PROCESSION_MODULE
+#include "traction.hpp"
+#include "trajection.hpp"
+#ifndef RAPBTL_NO_STD_CPLUSPLUS
+#include <type_traits>
+#endif
+
+namespace procession {
+
+    using namespace ::traction;
+    using namespace ::trajection;
+
+    using Contractual = void(
+        const Locational< void >
+    );
+
+    struct Contextual {
+
+        Locational< void >
+            location;
+
+        Locational< Contractual >
+            objective;
+
+    };
+
+    template <
+        typename Spatial,
+        typename Subjective
+    >
+    using ReferenceSchedular = bool(
+        Referential< Spatial >,
+        Referential< Subjective >
+    );
+
+    template <
+        typename Spatial,
+        typename Subjective
+    >
+    using PointerSchedular = bool(
+        Referential< Spatial >,
+        const Locational< Subjective >
+    );
+
+    template <
+        typename Spatial
+    >
+    using Processive = bool(
+        Referential< Spatial >
+    );
+
+    template <
+        typename Spatial,
+        typename Positional,
+        typename Natural,
+        typename Subjective,
+        Referential< const Tractile< Spatial, Positional, Natural > >
+            Contractor,
+        Referential< const Directional< Spatial, Positional, Natural, Contextual > >
+            Visitor,
+        Referential< Contractual >
+            Function
+    >
+    static inline bool
+    ScheduleByPointer(
+        Referential< Spatial >
+            space,
+        const Locational< Subjective >
+            subject
+    ) {
+#ifndef RAPBTL_NO_STD_CPLUSPLUS
+        using namespace ::std;
+        static_assert(
+            is_integral< Natural >::value && is_unsigned< Natural >::value,
+            "Natural:  Unsigned integer type required"
+        );
+#endif
+        using General = const Locational< void >;
+        Positional
+            position;
+        if (!Contractor.protract( space, position, 1 ))
+            return false;
+        Visitor.scale.go( space, position ).to.location = (General) subject;
+        Visitor.scale.go( space, position ).to.objective = Function;
+        return true;
+    }
+
+    template <
+        typename Spatial,
+        typename Positional,
+        typename Natural,
+        typename Subjective,
+        Referential< const Tractile< Spatial, Positional, Natural > >
+            Contractor,
+        Referential< const Directional< Spatial, Positional, Natural, Contextual > >
+            Visitor,
+        Referential< Contractual >
+            Function
+    >
+    static inline bool
+    ScheduleByReference(
+        Referential< Spatial >
+            space,
+        Referential< Subjective >
+            subject
+    ) {
+#ifndef RAPBTL_NO_STD_CPLUSPLUS
+        using namespace ::std;
+        static_assert(
+            is_integral< Natural >::value && is_unsigned< Natural >::value,
+            "Natural:  Unsigned integer type required"
+        );
+#endif
+        static auto&
+            Schedule = ScheduleByPointer< Spatial, Positional, Natural, Subjective, Contractor, Visitor, Function >;
+        return Schedule( space, Locate( subject ).at );
+    }
+
+    template <
+        typename Spatial,
+        typename Positional,
+        typename Natural,
+        Referential< const Tractile< Spatial, Positional, Natural > >
+            Contractor,
+        Referential< const Directional< Spatial, Positional, Natural, Contextual > >
+            Visitor
+    >
+    static inline bool
+    Process(
+        Referential< Spatial >
+            space
+    ) {
+#ifndef RAPBTL_NO_STD_CPLUSPLUS
+        using namespace ::std;
+        static_assert(
+            is_integral< Natural >::value && is_unsigned< Natural >::value,
+            "Natural:  Unsigned integer type required"
+        );
+#endif
+        Positional
+            position;
+        Contextual
+            event;
+        if (!Visitor.begins( space, 0 ))
+            return false;
+        Visitor.scale.begin( space, position, 0 );
+        event = Visitor.scale.go( space, position ).to;
+        Contractor.retract( space, 1 );
+        event.objective( event.location );
+        return true;
+    }
+
+};
+
+#endif
